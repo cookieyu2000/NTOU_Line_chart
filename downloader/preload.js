ebf613e6b1f16a385f10eb20e4989180ba2c73a2
Ping-Yen Yu <cookie2000891109@gmail.com>
2026-01-01 17:33:42 +0800

    Delete downloader directory

--- downloader/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("downloader", {
-  getPlatform: () => ipcRenderer.invoke("get-platform"),
-  downloadInstaller: (platformKey) => ipcRenderer.invoke("download-installer", platformKey),
-  onProgress: (handler) => ipcRenderer.on("download-progress", (_event, progress) => handler(progress)),
-});
